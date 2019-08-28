31cafefd58790582fad96e097b9ae1df02825da0
Nicholas Krantz <krantni@gmail.com>
2019-08-27 21:07:45 -0500

    adding problem 5

--- Problems/index.ts
+++ Problems/index.ts
@@ -2,5 +2,6 @@ import { problem1 } from './Problem_1';
 import { problem2 } from './Problem_2';
 import { problem3 } from './Problem_3';
 import { problem4 } from './Problem_4';
+import { problem5 } from './Problem_5';
 
-export default [problem1, problem2, problem3, problem4];
+export default [problem1, problem2, problem3, problem4, problem5];
